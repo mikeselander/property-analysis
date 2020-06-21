fe96abfb73fb64ab3e10448856f6ce25ddc731cf
Mike Selander <mikeselander@gmail.com>
2020-06-21 11:07:46 -0600

    Add localStorage ability to App state to presrve state between reloads Fixes #8

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,6 +7,7 @@ import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from '../theme';
+import useLocalStorage from '../shared/use-local-storage';
 
 import {
     CITY_DATA,
@@ -41,52 +42,48 @@ export interface ApplicationData {
  * Add ability to save results to localStorage
  */
 
-class App extends React.Component {
+const App = () => {
+    const [ state, copyValues ] = useLocalStorage(
+        'prop-data',
+        {
+            capEx: 5,
+            city: DEFAULT_CITY,
+            closingCosts: 3700,
+            hoa: 0,
+            insuranceCost: 50,
+            interestRate: STARTING_INTEREST_RATE,
+            maintenance: 5,
+            management: 11,
+            monthlyRent: 1000,
+            percentDown: 20,
+            pmi: false,
+            price: 100000,
+            repairCosts: 0,
+            taxRate: CITY_DATA[DEFAULT_CITY].taxRate,
+            typeOfRental: 'rental',
+            vacancy: CITY_DATA[DEFAULT_CITY].vacancy,
+        }
+    );
 
-    state: ApplicationData = {
-        capEx: 5,
-        city: DEFAULT_CITY,
-        closingCosts: 3700,
-        hoa: 0,
-        insuranceCost: 50,
-        interestRate: STARTING_INTEREST_RATE,
-        maintenance: 5,
-        management: 11,
-        monthlyRent: 1000,
-        percentDown: 20,
-        pmi: false,
-        price: 100000,
-        repairCosts: 0,
-        taxRate: CITY_DATA[DEFAULT_CITY].taxRate,
-        typeOfRental: 'rental',
-        vacancy: CITY_DATA[DEFAULT_CITY].vacancy,
-    };
+    console.table( state );
 
-    copyValues( values: object ) {
-        this.setState( values );
-    }
+    return (
+        <div className="App">
+            <ThemeProvider theme={ theme }>
+                <Container maxWidth="md">
 
-    render() {
-        console.table( this.state );
-
-        return (
-            <div className="App">
-                <ThemeProvider theme={theme}>
-                    <Container maxWidth="md">
-
-                        <Grid
-                            container
-                            alignItems="flex-start"
-                            justify="space-between"
-                        >
-                            <InputForm allValues={ this.state } copyValues={ ( state: object ) => this.copyValues( state ) }  />
-                            <Results allValues={ this.state } />
-                        </Grid>
-                    </Container>
-                </ThemeProvider>
-            </div>
-        );
-    }
+                    <Grid
+                        container
+                        alignItems="flex-start"
+                        justify="space-between"
+                    >
+                        <InputForm allValues={ state } copyValues={ ( state: object ) => copyValues( state ) }  />
+                        <Results allValues={ state } />
+                    </Grid>
+                </Container>
+            </ThemeProvider>
+        </div>
+    );
 }
 
 export default App;
